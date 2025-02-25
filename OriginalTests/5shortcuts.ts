5a2cfc480693e055163f17d3f25a0f996f61dee5
reecen9696 <snimorak@gmail.com>
2025-02-25 16:54:15 +1100

    training update

---
+++ OriginalTests/5shortcuts.ts
@@ -0,0 +1,96 @@
+// ========================
+// 🔎 1. Quick File Navigation
+// ========================
+// Quickly open another file in your project.
+// Cmd + P
+// 👉 Practice: Find SHORTCUTS.md
+
+// ========================
+// 📝 2. Highlighting & Selection
+// ========================
+// Highlight this line line below without touching the mouse & delete the line!
+// Shift + ↓
+console.log("DELETE ME!");
+
+// ========================
+// 📝 3. Highlighting & Selection
+// ========================
+// Place your cursor in the middle of the line below.
+// Shift + Option + →
+const selectionExample = "wordSelection";
+
+// ========================
+// 📝 4. Highlighting & Selection
+// ========================
+//Cmd + D
+// Select all occurrences of the word "duplicate" and rename them all at once.
+const duplicate = 1;
+const duplicateValue = duplicate + 2;
+console.log(duplicate);
+
+// ========================
+// 🖊️ 5. Renaming
+// ========================
+// Globe + F2
+// Place your cursor on the variable "renameMe" and rename it.
+const renameMe = "Old Name";
+console.log(renameMe);
+
+// ========================
+// 🧭 6. Line Navigation
+// ========================
+// Cmd + ←
+// Cmd + →
+// Jump to the start of this line and to jump to the end.
+
+// ========================
+// 🧭 7. Line Navigation
+// ========================
+// Cmd + ↑
+// Cmd + ↓
+// Move to the top of the file and then back to the bottom
+
+// ========================
+// 🖊️  8. Word Navigation
+// ========================
+//Option + →
+//Option + ←
+// Move through the sentence without the mouse.
+const sentence = "Move through these words quickly.";
+
+// ========================
+// 📝 9. Move Lines
+// ========================
+//Option + ↑ / ↓
+// Move this entire line up or down.
+console.log("Move this line up or down with the shortcut");
+
+// ========================
+// 📝 10. Move Paragrapgs
+// ========================
+// Command + ←
+// Shift + ↑ / ↓
+// Option + ↑ / ↓
+// Move this entire line up or down.
+console.log("Move this line up or down with the shortcut");
+
+// ========================
+// 🗑️ 11. Deletion
+// ========================
+// Option + Backspace
+// Delete the word 'removeMe' in the line below.
+const removeMe = "This should be deleted";
+
+// ========================
+// 🗑️ 12. Deletion
+// ========================
+// Cmd + Backspace
+// Delete this entire line.
+console.log("Delete this line using Cmd + Backspace");
+
+// ========================
+// 🗑️ 13. Deletion
+// ========================
+// Fn + Delete
+// Delete forward in the middle of this text.
+const forwardDelete = "Remove part of this text forward";
